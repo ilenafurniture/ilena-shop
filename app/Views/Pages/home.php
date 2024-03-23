ceacea1de98d4bd94e7a8dba6eba98688498ab94
Aditya Anugrah <akunmlggnibos@gmail.com>
2024-03-23 13:43:48 +0700

    update ilena navbar

--- app/Views/Pages/home.php
+++ app/Views/Pages/home.php
@@ -1,6 +1,14 @@
 <?= $this->extend("layout/template"); ?>
 <?= $this->section("content"); ?>
-<div>
-    <p>ini halaman home</p>
+<div class="konten">
+    <div>
+        <div class="img-teks">
+            <h1>ALD Lemari Dewasa</h1>
+            <h5>Lemari pilihan dan terbaik seIndonesia</h5>
+        </div>
+        <img class="img-besar"
+            src="https://images.unsplash.com/photo-1616486338812-3dadae4b4ace?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="">
+    </div>
 </div>
 <?= $this->endSection(); ?>
---
+++ app/Views/layout/navbar.php
@@ -0,0 +1,76 @@
+<nav class="nav" style="background-color: black;">
+    <div class="container py-3">
+        <div class="d-flex w-100 justify-content-between align-items-center">
+            <div style="width: calc(100% / 3)">
+                <form action="">
+                    <input placeholder="Cari produk" class="input" name="text" type="text">
+                </form>
+            </div>
+            <div style="width: calc(100% / 3)" class="d-flex justify-content-center">
+                <img src="/img/logoilenawhite.png" alt="logo ilena" height="30em">
+            </div>
+            <div style="width: calc(100% / 3)" class="d-flex justify-content-end">
+                <a href="/wishlist" class="btn"><i class="material-icons text-light">favorite_border</i></a>
+                <a href="/cart" class="btn"><i class="material-icons text-light">shopping_cart</i></a>
+                <a href="/account" class="btn"><i class="material-icons text-light">person_outline</i></a>
+            </div>
+        </div>
+    </div>
+    <div class="d-flex justify-content-center w-100" style="background-color:whitesmoke;">
+        <div class="d-flex align-items-center py-2" style="width:300px">
+            <div class="list-nav">
+                <a class="text-dark text-center w-100 d-block" style="text-decoration: none;" href="">Kursi</a>
+                <div class="child-list-nav">
+                    <div class="container py-4">
+                        <p class="m-0">Jelajahi Kursi</p>
+                        <a class="text-dark w-100 d-block" style="text-decoration: none; font-weight:500;"
+                            href="#">Kursi
+                            Belajar</a>
+                        <a class="text-dark w-100 d-block" style="text-decoration: none; font-weight:500;"
+                            href="#">Kursi
+                            TV</a>
+                    </div>
+                </div>
+            </div>
+            <div class="list-nav">
+                <a class="text-dark text-center w-100 d-block" style="text-decoration: none;" href="">Rak</a>
+                <div class="child-list-nav">
+                    <div class="container py-4">
+                        <p class="m-0">Jelajahi Rak</p>
+                        <a class="text-dark w-100 d-block" style="text-decoration: none; font-weight:500;" href="#">Rak
+                            Belajar</a>
+                        <a class="text-dark w-100 d-block" style="text-decoration: none; font-weight:500;" href="#">Rak
+                            TV</a>
+                    </div>
+                </div>
+            </div>
+            <div class="list-nav">
+                <a class="text-dark text-center w-100 d-block" style="text-decoration: none;">Meja</a>
+                <div class="child-list-nav">
+                    <div class="container py-4">
+                        <p class="m-0">Jelajahi Meja</p>
+                        <a class="text-dark w-100 d-block" style="text-decoration: none; font-weight:500;" href="#">Meja
+                            Belajar</a>
+                        <a class="text-dark w-100 d-block" style="text-decoration: none; font-weight:500;" href="#">Meja
+                            TV</a>
+                    </div>
+                </div>
+            </div>
+            <div class="list-nav">
+                <a class="text-dark text-center w-100 d-block" style="text-decoration: none;" href="">Lemari</a>
+                <div class="child-list-nav">
+                    <div class="container py-4">
+                        <p class="m-0">Jelajahi Lemari</p>
+                        <a class="text-dark w-100 d-block" style="text-decoration: none; font-weight:500;"
+                            href="#">Lemari
+                            Belajar</a>
+                        <a class="text-dark w-100 d-block" style="text-decoration: none; font-weight:500;"
+                            href="#">Lemari
+                            TV</a>
+                    </div>
+                </div>
+            </div>
+            <div class="bg-list-nav"></div>
+        </div>
+    </div>
+</nav>
