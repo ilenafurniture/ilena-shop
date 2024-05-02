d9054e59f48c2d5276bf82dace638a8adaf366d1
Aditya Anugrah <akunmlggnibos@gmail.com>
2024-05-02 15:38:55 +0700

    login register

--- app/Views/pages/login.php
+++ app/Views/pages/login.php
@@ -1,4 +1,43 @@
 <?= $this->extend("layout/template"); ?>
 <?= $this->section("content"); ?>
-
+<div style="flex:1">
+    <form action=" post" class="form-custom">
+        <div class="d-flex justify-content-center">
+            <div style="flex:1">
+                <h1>test</h1>
+            </div>
+            <div style="flex:1.5; padding: 5em;">
+                <h5 class="mb-4">Create Accounts</h5>
+                <div class="d-flex border-bottom gap-2 align-items-center mb-3">
+                    <i class="material-icons ">person_outline</i>
+                    <input type="email" placeholder="Masukan Email">
+                </div>
+                <div class=" d-flex border-bottom gap-2 align-items-center mb-3">
+                    <i class="material-icons ">lock</i>
+                    <input type="password" placeholder="Masukan password" id="passworku">
+                </div>
+                <div>
+                    <input type="checkbox" onclick="liatPassword()" id="showw">
+                    <label for="showw" style="font-size:small;" class="text-no-select">Show Password</label>
+                </div>
+                <div>
+                    <button class="btn-default">Membuat Akun</button>
+                </div>
+                <div>
+                    <p>Sudah mempunyai akun?<a href="">Masuk Sekarang</a></p>
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
+<script>
+function liatPassword() {
+    var x = document.getElementById("passworku");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
+</script>
 <?= $this->endSection(); ?>
--- app/Views/pages/wishlist.php
+++ app/Views/pages/wishlist.php
@@ -1,6 +1,6 @@
 <?= $this->extend("layout/template"); ?>
 <?= $this->section("content"); ?>
-<div class="container konten ">
+<div class="container konten">
     <div class="container d-flex justify-content-between mb-4">
         <div>
             <h3 class="teks-sedang">Wishlist (<?= count($wishlist) <= 0 ? '0' : count($wishlist) ?>)</h3>
@@ -12,48 +12,52 @@
     <div class="container mb-5">
         <div class="container-card1">
             <?php foreach ($produk as $ind_p => $p) { ?>
-                <div class="card1">
-                    <div style="position: relative;">
-                        <div class="card1-content-img">
-                            <span><?= $p['diskon'] > 0 ? $p['diskon'] . "%" : '' ?></span>
-                            <div class="d-flex flex-column gap-2">
-                                <?= in_array($p['id'], $wishlist) ? '<a class="card1-btn-img" href="/delwishlist/' . $p['id'] . '"><i class="material-icons">bookmark</i></a>' : '<a class="card1-btn-img" href="/addwishlist/' . $p['id'] . '"><i class="material-icons">bookmark_border</i></a>' ?>
-                                <a id="card<?= $ind_p ?>" class="card1-btn-img" href="/addcart/<?= $p['id'] ?>/<?= json_decode($p['varian'], true)[0]['nama'] ?>/1"><i class="material-icons">shopping_cart</i></a>
-                            </div>
+            <div class="card1">
+                <div style="position: relative;">
+                    <div class="card1-content-img">
+                        <span><?= $p['diskon'] > 0 ? $p['diskon'] . "%" : '' ?></span>
+                        <div class="d-flex flex-column gap-2">
+                            <?= in_array($p['id'], $wishlist) ? '<a class="card1-btn-img" href="/delwishlist/' . $p['id'] . '"><i class="material-icons">bookmark</i></a>' : '<a class="card1-btn-img" href="/addwishlist/' . $p['id'] . '"><i class="material-icons">bookmark_border</i></a>' ?>
+                            <a id="card<?= $ind_p ?>" class="card1-btn-img"
+                                href="/addcart/<?= $p['id'] ?>/<?= json_decode($p['varian'], true)[0]['nama'] ?>/1"><i
+                                    class="material-icons">shopping_cart</i></a>
                         </div>
-                        <a href="/product/<?= $p['id']; ?>">
-                            <img id="img<?= $ind_p ?>" src="/viewpic/<?= $p['id']; ?>" alt="">
-                        </a>
                     </div>
-                    <div class="container-varian mb-1">
-                        <?php foreach (json_decode($p['varian'], true) as $ind_v => $v) { ?>
-                            <input id="varian-<?= $ind_p ?>-<?= $ind_v ?>" value="<?= $v['urutan_gambar'] ?>-<?= $v['nama'] ?>" type="radio" name="varian<?= $ind_p ?>">
-                            <label for="varian-<?= $ind_p ?>-<?= $ind_v ?>"><span style="background-color: <?= $v['kode'] ?>"></span></label>
-                        <?php } ?>
-                        <script>
-                            const btnKeranjang<?= $ind_p ?>Elm = document.getElementById("card<?= $ind_p ?>");
-                            const varian<?= $ind_p ?>Elm = document.querySelectorAll('input[name="varian<?= $ind_p ?>"]');
-                            varian<?= $ind_p ?>Elm.forEach(elm => {
-                                elm.addEventListener('change', (e) => {
-                                    console.log(e.target.value)
-                                    const img<?= $ind_p ?>Elm = document.getElementById("img<?= $ind_p ?>");
-                                    img<?= $ind_p ?>Elm.src =
-                                        "/viewvar/<?= $p['id']; ?>/" + e.target.value.split("-")[0].split(",")[
-                                            0];
-
-                                    btnKeranjang<?= $ind_p ?>Elm.href = "/addcart/<?= $p['id'] ?>/" + e.target
-                                        .value.split("-")[1] + "/1";
-                                })
-                            });
-                        </script>
-                    </div>
-                    <h5><?= $p['nama']; ?></h5>
-                    <div class="d-flex gap-2">
-                        <p class="harga">Rp <?= number_format($p['harga'] * (100 - $p['diskon']) / 100, 0, ',', '.'); ?></p>
-                        <p class="harga-diskon">Rp <?= number_format($p['harga'], 0, ',', '.') ?>
-                        </p>
-                    </div>
+                    <a href="/product/<?= $p['id']; ?>">
+                        <img id="img<?= $ind_p ?>" src="/viewpic/<?= $p['id']; ?>" alt="">
+                    </a>
                 </div>
+                <div class="container-varian mb-1">
+                    <?php foreach (json_decode($p['varian'], true) as $ind_v => $v) { ?>
+                    <input id="varian-<?= $ind_p ?>-<?= $ind_v ?>" value="<?= $v['urutan_gambar'] ?>-<?= $v['nama'] ?>"
+                        type="radio" name="varian<?= $ind_p ?>">
+                    <label for="varian-<?= $ind_p ?>-<?= $ind_v ?>"><span
+                            style="background-color: <?= $v['kode'] ?>"></span></label>
+                    <?php } ?>
+                    <script>
+                    const btnKeranjang<?= $ind_p ?>Elm = document.getElementById("card<?= $ind_p ?>");
+                    const varian<?= $ind_p ?>Elm = document.querySelectorAll('input[name="varian<?= $ind_p ?>"]');
+                    varian<?= $ind_p ?>Elm.forEach(elm => {
+                        elm.addEventListener('change', (e) => {
+                            console.log(e.target.value)
+                            const img<?= $ind_p ?>Elm = document.getElementById("img<?= $ind_p ?>");
+                            img<?= $ind_p ?>Elm.src =
+                                "/viewvar/<?= $p['id']; ?>/" + e.target.value.split("-")[0].split(",")[
+                                    0];
+
+                            btnKeranjang<?= $ind_p ?>Elm.href = "/addcart/<?= $p['id'] ?>/" + e.target
+                                .value.split("-")[1] + "/1";
+                        })
+                    });
+                    </script>
+                </div>
+                <h5><?= $p['nama']; ?></h5>
+                <div class="d-flex gap-2">
+                    <p class="harga">Rp <?= number_format($p['harga'] * (100 - $p['diskon']) / 100, 0, ',', '.'); ?></p>
+                    <p class="harga-diskon">Rp <?= number_format($p['harga'], 0, ',', '.') ?>
+                    </p>
+                </div>
+            </div>
             <?php } ?>
         </div>
     </div>
