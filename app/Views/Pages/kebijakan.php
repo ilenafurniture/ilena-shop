bfb57b72883a5ba91f7732d024ba4db9b5422891
Aditya Anugrah <akunmlggnibos@gmail.com>
2024-05-07 15:11:48 +0700

    Update Again

--- app/Views/Pages/kebijakan.php
+++ app/Views/Pages/kebijakan.php
@@ -1,5 +1,58 @@
 <?= $this->extend("layout/template"); ?>
 <?= $this->section("content"); ?>
 <div class="container konten">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h3>Kebijakan Privasi</h3>
+                <p><strong>Terakhir Diperbarui:</strong> Mei 2024</p>
+
+                <h5>Informasi yang Kami Kumpulkan</h5>
+                <p>Ketika Anda mengunjungi situs web kami, kami dapat mengumpulkan informasi pribadi yang Anda berikan
+                    secara sukarela, seperti nama, alamat email, alamat rumah, nomor telepon, dan informasi pembayaran.
+                    Selain itu, kami juga dapat mengumpulkan informasi non-pribadi, seperti browser yang Anda gunakan,
+                    jenis perangkat, alamat IP, dan data demografis lainnya.</p>
+
+                <h5>Penggunaan Informasi</h5>
+                <p>Informasi yang kami kumpulkan digunakan untuk keperluan berikut:</p>
+                <ol>
+                    <li>Memproses pesanan dan pembayaran Anda.</li>
+                    <li>Mengirimkan konfirmasi pesanan dan pemberitahuan pengiriman.</li>
+                    <li>Menyediakan layanan pelanggan dan dukungan teknis.</li>
+                    <li>Mengirimkan informasi promosi dan penawaran khusus, jika Anda telah menyetujui menerima
+                        komunikasi tersebut.</li>
+                </ol>
+
+                <h5>Pemakaian Cookie</h5>
+                <p>Situs web kami dapat menggunakan cookie dan teknologi pelacakan serupa untuk meningkatkan pengalaman
+                    pengguna Anda. Cookie adalah file kecil yang disimpan di komputer Anda yang memungkinkan kami untuk
+                    mengenali perangkat Anda dan menyimpan preferensi Anda.</p>
+
+                <h5>Keamanan Informasi</h5>
+                <p>Kami mengambil langkah-langkah keamanan yang wajar untuk melindungi informasi pribadi Anda dari akses
+                    yang tidak sah, penggunaan, atau pengungkapan yang tidak sah. Namun, tidak ada metode transmisi atau
+                    penyimpanan data yang aman secara absolut.</p>
+
+                <h5>Pembagian Informasi</h5>
+                <p>Kami tidak akan menjual, menyewakan, atau menukar informasi pribadi Anda kepada pihak ketiga tanpa
+                    izin Anda, kecuali yang diperlukan untuk memproses pesanan Anda atau atas permintaan hukum.</p>
+
+                <h5>Perubahan Kebijakan Privasi</h5>
+                <p>Kami dapat memperbarui kebijakan privasi ini dari waktu ke waktu dengan memposting perubahan di
+                    halaman ini. Anda disarankan untuk memeriksa halaman ini secara berkala untuk melihat pembaruan atau
+                    perubahan.</p>
+
+                <h5>Pertanyaan dan Kontak</h5>
+                <p>Jika Anda memiliki pertanyaan tentang kebijakan privasi kami, silakan hubungi kami di <a
+                        href="info@ilenafurniture.com">info@ilenafurniture.com</a> atau melalui formulir kontak di situs
+                    web
+                    kami.</p>
+
+                <p>Dengan menggunakan situs web kami, Anda menyetujui pengumpulan dan penggunaan informasi sebagaimana
+                    diatur dalam kebijakan privasi ini.</p>
+            </div>
+        </div>
+    </div>
+
 </div>
 <?= $this->endSection(); ?>
--- app/Views/Pages/tentang.php
+++ app/Views/Pages/tentang.php
@@ -1,5 +1,82 @@
 <?= $this->extend("layout/template"); ?>
 <?= $this->section("content"); ?>
 <div class="container konten">
+    <div class="container mb-3">
+        <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="/">Home</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Tentang</li>
+            </ol>
+        </nav>
+        <h1>
+            Sejarah Perusahaan
+        </h1>
+        <h5 class="mb-5">Ilena Furniture</h5>
+        <div class="baris-ke-kolom-reverse align-items-center justify-content-between">
+            <div style="height: fit-content; flex: 2;">
+                <p>
+                    Ilena Furniture hadir sejak tahun 1989 sebagai produsen sekaligus distributor aneka perabotan
+                    rumah yang berpengalaman. Selama lebih dari tiga puluhan tahun yang lalu hingga sekarang, Jasmine
+                    Furniture dengan konsisten memproduksi perabotan mulai dari meja, kursi, lemari dan rak dengan
+                    kualitas terbaik yang terbuat dari material terbaik yang dikerjakan secara professional oleh tim
+                    terbaik kami.
+                </p>
+                <p>
+                    Saat ini kami telah memiliki ratusan distributor dan toko ternama, yang tersebar di seluruh penjuru
+                    kota di Indonesia.
+                </p>
+                <p>
+                    Untuk semakin memudahkan aktivitas belanja konsumen, kami melakukan pengembangan dan hadir di
+                    berbagai platform marketplace ternama di Indonesia.
+                </p>
+                <p>
+                    Temukan produk furniture kebutuhan Anda dengan harga terbaik dan desain stylish masa kini, di toko
+                    terdekat Anda bersama dengan Ilena Furniture.
+                </p>
+                <p class="fw-bold" style="color: var(--hijau)">
+                    Ilena Furniture, Always your choice, always your furniture
+                </p>
+            </div>
+            <div class="show-ke-hide align-items-center justify-content-end" style="height: 350px; flex: 1;">
+                <div style="
+                        height: 100%;
+                        width: 100%;
+                        display: grid;
+                        gap: 1em;
+                        grid-template-areas: 'gmbr1 gmbr3' 'gmbr2 gmbr3' 'gmbr2 gmbr4';
+                    ">
+                    <div style="
+                            grid-area: gmbr1;
+                            border-radius: 1em;
+                            overflow: hidden;
+                        ">
+                        <img style="height: 100%; width: 100%; object-fit: cover" src="/img/header/header_comp3.webp" />
+                    </div>
+                    <div style="
+                            grid-area: gmbr2;
+                            border-radius: 1em;
+                            overflow: hidden;
+                        ">
+                        <img style="height: 100%; width: 100%; object-fit: cover" src="/img/header/header_comp1.webp" />
+                    </div>
+                    <div style="
+                            grid-area: gmbr3;
+                            border-radius: 1em;
+                            overflow: hidden;
+                        ">
+                        <img style="height: 100%; width: 100%; object-fit: cover" src="/img" />
+                    </div>
+                    <div style="
+                            grid-area: gmbr4;
+                            border-radius: 1em;
+                            overflow: hidden;
+                        ">
+                        <img style="height: 100%; width: 100%; object-fit: cover" src="/img/header/header_comp4.webp" />
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
 </div>
 <?= $this->endSection(); ?>
