4a9135cba62cec1256b18da9fe6b86ecf9c86356
Aditya Anugrah <akunmlggnibos@gmail.com>
2024-05-10 11:26:34 +0700

    update fonENd

--- app/Views/Pages/syarat.php
+++ app/Views/Pages/syarat.php
@@ -1,5 +1,66 @@
 <?= $this->extend("layout/template"); ?>
 <?= $this->section("content"); ?>
 <div class="container konten">
+    <div class="container">
+        <h3 class="mb-1">Syarat Dan Ketentuan</h3>
+        <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="/">Home</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Syarat dan Ketentuan</li>
+            </ol>
+        </nav>
+
+        <h5>Pendahuluan</h5>
+        <p>Perjanjian penggunaan website <a style="color: var(--hijau);"
+                class="link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover"
+                href="https://ilenafurniture.com">ilenafurniture.com</a> selaku pengelola (disebut sebagai
+            “kami”)
+            engan pengguna dijelaskan pada halaman ini. seluruh pengguna wajib untuk mengikuti syarat dan
+            ketentuan yang telah ditentukan. Kami memiliki hak untuk mengubah atau menambahkan suatu pasal
+            sesuai dengan keperluan website.</p>
+
+        <h5>Penggunaan Website</h5>
+        <p>Situs website <a style="color: var(--hijau);"
+                class="link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover"
+                href="https://ilenafurniture.com">ilenafurniture.com</a> memuat informasi yang meliputi: produk,
+            kebijakan privasi, FAQ,
+            serta syarat & ketentuan.</p>
+
+        <h5>Profil Perusahaan</h5>
+        <p>Informasi yang terdapat dalam profil Perusahaan, serta kegiatan Perusahaan dapat berubah dan atau
+            ditambahkan sesuai dengan kebutuhan Perusahaan di kemudian hari.
+        </p>
+
+        <h5>Informasi Produk dan Inspirasi</h5>
+        <p>Semua informasi yang menyangkut tentang harga, gambar, deskripsi, dan spesifikasi dapat berubah dalam kurun
+            waktu tertentu. Harga yang digunakan pada website ini berlaku untuk semua wilayah jangkauan dari produk
+            jasmine
+            furniture, dengan transaksi jual-beli dilakukan pada website ini. produk yang ditampilkan di website berlaku
+            selama persediaan masih ada. Silahkan hubungi customer service kami untuk informasi lebih lanjut seputar
+            produk
+            atau pertanyaan terkait.</p>
+        <p>Tidak diperkenankan bagi pengguna untuk menggunakan, mencetak, atau menyebarluaskan informasi di dalam
+            website ini
+            untuk kepentingan maupun keuntungan pribadi dan atau kelompok.</p>
+        <p>Customer service kami selalu bersedia dengan terbuka dan senang hati untuk melakukan pelayanan kepada
+            pelanggan pada
+            hari senin-jumat pukul 08.00-16.00 WIB hingga sabtu pukul 08.00-14.00 WIB (kecuali hari minggu dan libur
+            nasional).</p>
+
+        <h5>Informasi Promosi</h5>
+        <p>Pengguna dapat melihat informasi promosi yang berlangsung secara berkala atau dalam waktu tertentu yang dapat
+            berubah
+            sewaktu-waktu. Pengguna disarankan untuk berlangganan email atau WhatsApp untuk selanjutnya kami akan
+            mengirimkan
+            informasi promosi terbaru yang sedang kami adakan.</p>
+
+        <h5>Informasi Service</h5>
+        <p>Pengguna dapat mengakses informasi seputar layanan yang diberikan oleh Ilena Furniture melalui <a
+                style="color: var(--hijau);"
+                class="link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover"
+                href="https://ilenafurniture.com">ilenafurniture.com</a>
+            atau WA chat (08112938160), informasi ini dapat berubah sewaktu- waktu sesuai dengan kebijakan Perusahaan
+            yang berlaku.</p>
+    </div>
 </div>
 <?= $this->endSection(); ?>
