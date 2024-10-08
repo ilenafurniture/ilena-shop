5c4de81e716d3c0f83776b32e414d0a2f4e1d59d
Aditya Anugrah <134223672+AdiityaAnugrah@users.noreply.github.com>
2024-10-08 11:09:06 +0700

    Update Navbar

--- app/Views/Pages/artikelAll.php
+++ app/Views/Pages/artikelAll.php
@@ -21,9 +21,7 @@
                     <a href="/all" class="btn btn-default-putih">Pergi ke Toko</a>
                 </div>
             </div> -->
-            <img class="d-block rounded header"
-                src="https://images.unsplash.com/photo-1613575831056-0acd5da8f085?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                alt="">
+            <img class="d-block rounded header" src="https://i.ibb.co.com/PYgCVJP/bennerilena.webp" alt="">
         </div>
         <?php
         $indexAwal = -1;
