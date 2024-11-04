5e336f66c51afc4ef9686d277920029dbc044f98
Aditya Anugrah <akunmlggnibos@gmail.com>
2024-11-04 15:16:31 +0700

    hapus pages

--- app/Views/pages/tracking.php
+++
@@ -1,9 +0,0 @@
-<?= $this->extend("layout/template"); ?>
-<?= $this->section("content"); ?>
-<div class="container konten">
-    <div>
-        <p></p>
-    </div>
-
-</div>
-<?= $this->endSection(); ?>
