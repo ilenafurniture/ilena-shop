3a8e693d4b496fe3d2937c841e9f022ef6c43b53
Aditya Anugrah <134223672+AdiityaAnugrah@users.noreply.github.com>
2024-12-09 14:25:25 +0700

    Update template again

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -53,28 +53,6 @@
     </script>
     <!-- TikTok Pixel Code End -->
     <meta name="google-site-verification" content="fbuobh8O6hdzi1dFtDU48hqyMlHD0xvCfObbr1EgD0M" />
-
-    <!-- Google Tag Manager -->
-    <!-- <script>
-    (function(w, d, s, l, i) {
-        w[l] = w[l] || [];
-        w[l].push({
-            'gtm.start': new Date().getTime(),
-            event: 'gtm.js'
-        });
-        var f = d.getElementsByTagName(s)[0],
-            j = d.createElement(s),
-            dl = l != 'dataLayer' ? '&l=' + l : '';
-        j.async = true;
-        j.src =
-            'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-        f.parentNode.insertBefore(j, f);
-    })(window, document, 'script', 'dataLayer', 'GTM-KTNC9FL7');
-    </script> -->
-    <!-- End Google Tag Manager -->
-
-    <!-- Google tag (gtag.js) -->
-    <!-- <script async src="https://www.googletagmanager.com/gtag/js?id=G-6Q18QTYQE0"></script> -->
     <script>
     window.dataLayer = window.dataLayer || [];
 
