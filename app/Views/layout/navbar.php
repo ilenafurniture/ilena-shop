8339c61198de3785f74eaa10bc659740e83e2454
Aditya Anugrah <134223672+AdiityaAnugrah@users.noreply.github.com>
2025-03-17 08:55:05 +0700

    update link react

--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -113,7 +113,7 @@
 
 
 <script crossorigin src="https://unpkg.com/react@17/umd/react.production.min.js"></script>
-<script crossorigin src="https://unpkg.com/react-dom@17/umd/react-dom.production.min.js"></script>s
+<script crossorigin src="https://unpkg.com/react-dom@17/umd/react-dom.production.min.js"></script>
 <!-- <script src="https://unpkg.com/react@17/umd/react.development.js" crossorigin></script>
 <script src="https://unpkg.com/react-dom@17/umd/react-dom.development.js" crossorigin></script> -->
 <script crossorigin src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
