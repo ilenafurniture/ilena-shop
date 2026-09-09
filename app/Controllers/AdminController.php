e83bd73c49d2272b415d73d9ef2124d01d76f947
AdiityaAnugrah <akunmlggnibos@gmail.com>
2026-09-09 11:33:58 +0700

    Sync admin cover thumbnail update

--- app/Controllers/AdminController.php
+++ app/Controllers/AdminController.php
@@ -555,6 +555,35 @@ class AdminController extends BaseController
             $varian = $data['varian'] ?? $barang['varian'];
             if (is_array($varian)) $varian = json_encode($varian);
 
+            // Cover/thumbnail katalog mengikuti foto pertama dari urutan varian pertama.
+            // Jadi kalau file foto 5 digeser menjadi posisi #1, cover ikut memakai file 5.
+            $varianArrForThumb = json_decode((string)$varian, true) ?: [];
+            $firstImageSlot = null;
+            if (!empty($varianArrForThumb[0]['urutan_gambar'])) {
+                $slots = array_values(array_filter(array_map('trim', explode(',', (string)$varianArrForThumb[0]['urutan_gambar']))));
+                $firstImageSlot = isset($slots[0]) ? (int)$slots[0] : null;
+            }
+            if ($firstImageSlot) {
+                $thumbSource1000 = "img/barang/1000/{$id_product}-{$firstImageSlot}.webp";
+                $thumbSource3000 = "img/barang/3000/{$id_product}-{$firstImageSlot}.webp";
+                $thumbSource = is_file($thumbSource1000) ? $thumbSource1000 : (is_file($thumbSource3000) ? $thumbSource3000 : null);
+                if ($thumbSource) {
+                    $thumbDest = "img/barang/300/{$id_product}.webp";
+                    $thumbTmp = "img/barang/300/.tmp-thumb-" . uniqid('', true) . ".webp";
+                    \Config\Services::image()
+                        ->withFile($thumbSource)
+                        ->resize(300, 300, true, 'height')
+                        ->save($thumbTmp);
+                    if (is_file($thumbTmp) && filesize($thumbTmp) > 0) {
+                        @unlink($thumbDest);
+                        @rename($thumbTmp, $thumbDest);
+                        @touch($thumbDest);
+                    } else {
+                        @unlink($thumbTmp);
+                    }
+                }
+            }
+
             $boolTo01 = fn($v) => (is_bool($v) ? ($v ? '1' : '0') : (string)$v);
 
             $payload = [
